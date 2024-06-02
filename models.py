6ab3b7fb48c2f06fe79ddf2b38f434c49d37ad97
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-02 23:08:31 +0530

    added fields under products

--- models.py
+++ models.py
@@ -7,4 +7,7 @@ class Products(Model):
     name=fields.CharField(max_length=30,nullable=False)
     quantity_in_stock=fields.IntField(default=0)
     quantity_sold=fields.IntField(default=0)
-    
+    unit_price=fields.DecimalField(max_digit=8,decimal_places=2,default=0.00)
+    revenue=fields.DecimalField(max_digit=20,decimal_places=2,default=0.00)
+    supplied_by=fields.ForeignKeyField('models.supplier',related_name="goods.supplier")
+   
