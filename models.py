5f02af52fba679ad7b9072de6d57b9d9992625ca
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-02 23:02:36 +0530

    created a model called products

--- models.py
+++ models.py
@@ -1,2 +1,10 @@
 from tortoise.models import Model
 from tortoise import fields
+
+
+class Products(Model):
+    id=fields.IntField(pk=True)
+    name=fields.CharField(max_length=30,nullable=False)
+    quantity_in_stock=fields.IntField(default=0)
+    quantity_sold=fields.IntField(default=0)
+    
