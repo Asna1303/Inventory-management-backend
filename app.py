53ff4793d6d254d8569628157ba36ae0de708113
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-03 19:57:48 +0530

    Initialize Tortoise ORM with SQLite database

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app=FastAPI()
 @app.get('/')
 def index():
     return{"Msg":"Hello"}
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    modules={"models":["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
