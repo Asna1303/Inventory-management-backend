495541785d553b9dbb4a5a5e4e50044491adaa93
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-03 21:22:24 +0530

    imported suppplier class

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from models import (supplier_pydantic,Supplier_pydanticIn)
+from models import (supplier_pydantic,Supplier_pydanticIn,Supplier)
 
 app=FastAPI()
 @app.get('/')
 def index():
     return{"Msg":"go to /docs for the api documentation"}
+
+
+@app.post('/supplier')
+async def add_supplier(supplier_info:Supplier_pydanticIn):
+    supplier_obj=await
+
+
 
 register_tortoise(
     app,
