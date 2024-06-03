6099594a4e230dd71048e054959108b19c969952
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-03 23:46:36 +0530

    added api endpoint to delete supplier info by id

--- app.py
+++ app.py
@@ -29,15 +29,18 @@ async def get_specific_supplier(supplier_id:int):
 async def update_supplier(supplier_id:int,update_info:supplier_pydanticIn):
     supplier=await Supplier.get(id=supplier_id)
     update_info=update_info.dict(exclude_unset=True)
-    supplier.name=update_info.name
-    supplier.company=update_info.company
-    supplier.phone=update_info.phone
-    supplier.email=update_info.email
+    supplier.name=update_info['name']
+    supplier.company=update_info['company']
+    supplier.phone=update_info['phone']
+    supplier.email=update_info['email']
     await supplier.save()
-    response=supplier_pydantic .from_tortoise_orm(supplier)
+    response= await supplier_pydantic .from_tortoise_orm(supplier)
     return {"status": "OK", "data": response}
 
-
+@app.delete('/supplier/{supplier.id}')
+async def delete_supplier(supplier_id:int):
+    await Supplier.get(id=supplier_id).delete()
+    return {"status": "OK"}
 
 
 
