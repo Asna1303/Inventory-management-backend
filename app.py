c5098c88bf8ae6d45632450ed155fd26e87425a8
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-03 21:35:48 +0530

    Add /supplier endpoint to create and return supplier

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from models import (supplier_pydantic,Supplier_pydanticIn,Supplier)
+from models import supplier_pydantic, supplier_pydanticIn, Supplier  # Corrected import statement
 
-app=FastAPI()
+app = FastAPI()
+
 @app.get('/')
 def index():
-    return{"Msg":"go to /docs for the api documentation"}
-
+    return {"Msg": "go to /docs for the API documentation"}
 
 @app.post('/supplier')
-async def add_supplier(supplier_info:Supplier_pydanticIn):
-    supplier_obj=await
-
-
+async def add_supplier(supplier_info: supplier_pydanticIn):
+    supplier_obj = await Supplier.create(**supplier_info.dict(exclude_unset=True))
+    response = await supplier_pydantic.from_tortoise_orm(supplier_obj)
+    return {"status": "OK", "data": response}
 
 register_tortoise(
     app,
     db_url="sqlite://database.sqlite3",
-    modules={"models":["models"]},
+    modules={"models": ["models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
