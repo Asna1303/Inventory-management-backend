d0ab6e022147ec86daab6b8fffd97ad0e70024ef
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-05 09:11:15 +0530

    imported dotenv to app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from fastapi import BackgroundTasks, FastAPI
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 from pydantic import BaseModel, EmailStr
 from starlette.responses import JSONResponse
-
+#dotenv
+from dotenv import dotenv_values
 app = FastAPI()
 
 @app.get('/')
